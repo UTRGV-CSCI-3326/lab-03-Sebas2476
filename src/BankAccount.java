f9607eb2952d32b96d334298182640ea04ef7c03
Sebas2476 <Sebastian_ramos5@outlook.com>
2025-02-01 02:34:02 +0000

    Third Commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,12 @@
-
+public class BankAccount {
+    public static void main(String[] args) {
+        float Checking_Account = 2175.37f;
+        Checking_Account -= 302.50f;
+        Checking_Account += 50.03f;
+        Checking_Account = Checking_Account / 2 + 20.00f;
+        Checking_Account -= 1;
+        Checking_Account = Checking_Account * 2;
+        Checking_Account += 1;
+        System.out.printf("Bob's Updated Balance: %.2f\n", Checking_Account);
+    }
+}
